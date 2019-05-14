e3d5ef9e03ddeca6f7ea692ae58289719829c8eb
Adam Ahrens <adam.aahrens@gmail.com>
2019-05-13 20:57:19 -0500

    Handling creating a Chef

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,31 @@
 class ChefsController < ApplicationController
+  before_action :set_chef, only: %i[show]
+
+  def show
+  end
+
   def new
     @chef = Chef.new
+  end
+
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = 'Chef created successfully'
+      redirect_to chef_path(@chef)
+    else
+      logger.debug "Chef errors #{@chef.errors.full_messages}"
+      render 'new'
+    end
+  end
+
+  private
+
+  def chef_params
+    params.require(:chef).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_chef
+    @chef = Chef.find(params[:id])
   end
 end
--- test/integration/signup_test.rb
+++ test/integration/signup_test.rb
@@ -5,4 +5,21 @@ class SignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_response :success
   end
+
+  test 'reject an invalid signup form' do
+    get signup_path
+    assert_template 'chefs/new'
+    assert_no_difference 'Chef.count' do
+      post chefs_path, params: { chef: { name: ' ', email: ' ', password: ' ', password_confirmation: ' ' } }
+    end
+    assert_template 'chefs/new'
+  end
+
+  test 'accept a valid signup form' do
+    get signup_path
+    assert_template 'chefs/new'
+    assert_difference 'Chef.count', 1  do
+      post chefs_path, params: { chef: { name: 'Leroy Jeknis', email: 'leroy.jenkins@email.com', password: 'password1234', password_confirmation: 'password1234' } }
+    end
+  end
 end
